adcead276f87f5abd736167d19276394d28367a8
spacecodeur <spacecodeur@gmail.com>
2025-05-19 14:25:11 +0200

    algo: step 1

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -10,8 +10,15 @@ Exemples :
 */
 
 function countLetters(givenString: string, letter: string): number {
-  // Ton code ici !
-  return 0;
+  let result = 0;
+
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      result++;
+    }
+  }
+
+  return result;
 }
 
 export default countLetters;
