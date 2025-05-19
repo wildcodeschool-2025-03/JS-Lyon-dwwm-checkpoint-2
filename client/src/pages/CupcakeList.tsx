e2489b3e9f2ae27f16afe0c378ed0c3a59c50a4f
spacecodeur <spacecodeur@gmail.com>
2025-05-19 15:43:55 +0200

    web: step 4

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -41,7 +41,11 @@ function CupcakeList() {
           Filter by{" "}
           <select id="cupcake-select">
             <option value="">---</option>
-            {/* Step 4: add an option for each accessory */}
+            {accessories.map((accessory) => (
+              <option key={accessory.id} value={accessory.id}>
+                {accessory.name}
+              </option>
+            ))}
           </select>
         </label>
       </form>
