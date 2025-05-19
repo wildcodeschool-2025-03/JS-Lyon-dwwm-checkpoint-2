32217a20f8c66786eb6469cb8f12f7440c73291e
spacecodeur <spacecodeur@gmail.com>
2025-05-19 14:46:00 +0200

    algo: step 2

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -11,8 +11,24 @@ Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
 function getFibonacciSequence(size: number): number[] {
+  let result: number[] = [];
+
+  if (size <= 0) {
+    result = [];
+  } else if (size === 1) {
+    result = [0];
+  } else if (size === 2) {
+    result = [0, 1];
+  } else {
+    result = [0, 1];
+
+    for (let i = 0; i < size - 2; i++) {
+      result.push(result[i] + result[i + 1]);
+    }
+  }
+
   // Ton code ici !
-  return [0, 1];
+  return result;
 }
 
 export default getFibonacciSequence;
