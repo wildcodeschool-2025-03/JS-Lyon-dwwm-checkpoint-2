fc04fbe52ab6346c3ecdfcd5612ce4d57a410aa9
Jo-Quattro <Jordan.aulagnier@gmail.com>
2025-05-19 12:00:31 +0200

    step 4 done

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -48,7 +48,11 @@ function CupcakeList() {
           {/* Step 5: use a controlled component for select */}
           Filter by{" "}
           <select id="cupcake-select">
-            <option value="">---</option>
+            {accessories.map((accessorie) => (
+              <option key={accessorie.id} value="">
+                {accessorie.name}
+              </option>
+            ))}
             {/* Step 4: add an option for each accessory */}
           </select>
         </label>
